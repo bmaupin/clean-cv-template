f3a8da5542b38331e9c2126923215e5edf0be90c
bmaupin <bmaupin@users.noreply.github.com>
2018-07-30 12:57:50 -0400

    Wait 1 second when building to allow fonts to load

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 for htmlfile in *.html; do
-    google-chrome-stable --headless --no-sandbox --disable-gpu --print-to-pdf file://$(pwd)/$htmlfile
+    google-chrome-stable --headless --no-sandbox --disable-gpu --print-to-pdf file://$(pwd)/$htmlfile --virtual-time-budget=1000
     mv output.pdf "${htmlfile%.html}.pdf"
 done
