d748e4e41395c27b8060773265492e10e6760fcf
bmaupin <bmaupin@users.noreply.github.com>
2018-08-01 13:48:53 -0400

    Switch to node build script
    This gives us way more options than chrome headless and avoids some hacks

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for htmlfile in *.html; do
-    google-chrome-stable --headless --no-sandbox --disable-gpu --print-to-pdf file://$(pwd)/$htmlfile --virtual-time-budget=1000
-    mv output.pdf "${htmlfile%.html}.pdf"
-done
