2ef13dc5e890a408860c705577eab83e6c7452af
bmaupin <bmaupin@users.noreply.github.com>
2018-08-01 14:28:54 -0400

    Print all html files in cwd
    (Same behaviour as the previous build.sh script)

--- build.js
+++ build.js
@@ -1,5 +1,9 @@
+const fs = require('fs');
 const path = require('path');
 const puppeteer = require('puppeteer');
+
+const INPUT_EXTENSION = '.html';
+const OUTPUT_EXTENSION = '.pdf';
 
 // https://superuser.com/a/1250638/93066
 function timeout(ms) {
@@ -7,13 +11,38 @@ function timeout(ms) {
 };
 
 // https://developers.google.com/web/updates/2017/04/headless-chrome#screenshots
-(async() => {
-const browser = await puppeteer.launch();
-const page = await browser.newPage();
-await page.goto(`file:${path.join(__dirname, 'cv.html')}`);
-// Waiting allows time for the remote fonts to load
-await timeout(1000);
-await page.pdf({path: 'cv.pdf', format: 'letter'});
+async function printToPdf(browser, inputUrl) {
+  const outputFilename = path.basename(inputUrl, INPUT_EXTENSION) + OUTPUT_EXTENSION;
+  const page = await browser.newPage();
 
-await browser.close();
+  await page.goto(inputUrl);
+  // Waiting allows time for the remote fonts to load
+  await timeout(1000);
+  await page.pdf({path: outputFilename, format: 'letter'});
+}
+
+async function printFilesToPdf(files) {
+  const browser = await puppeteer.launch();
+
+  for (let i = 0; i < files.length; i++) {
+    const filename = files[i];
+    if (filename.endsWith(INPUT_EXTENSION)) {
+      await printToPdf(browser, `file:${path.join(__dirname, filename)}`);
+    }
+  }
+  await browser.close();
+}
+
+function getFilesInCwd() {
+  return new Promise((resolve, reject) => {
+    fs.readdir(__dirname, (err, files) => {
+      if (err) reject(err);
+      resolve(files);
+    })
+  })
+}
+
+(async() => {
+  const filesInCwd = await getFilesInCwd();
+  await printFilesToPdf(filesInCwd);
 })();
