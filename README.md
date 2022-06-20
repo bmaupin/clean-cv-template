a2459b2b70a200ff8ecf2762de96e81bf304bfdd
bmaupin <bmaupin@users.noreply.github.com>
2022-06-20 13:44:12 -0400

    feat: Pass input file path to build script
    This should make it more flexible. Also add instructions on creating the file in a browser, and add a small section on 'why'.

--- README.md
+++ README.md
@@ -3,34 +3,47 @@
 - Inspired by [moderncv](https://github.com/xdanaux/moderncv)
 - Filler text from [Anguish Languish](https://www.crockford.com/wrrrld/anguish.html)
 
+## Why?
 
-### Instructions
+I used to write my CV in LaTeX using the popular [moderncv](https://github.com/xdanaux/moderncv) template. However, if I wanted to change the formatting of my CV I would have a difficult time because I didn't use LaTeX for anything else. So I thought it would be much nicer to write my CV in technologies that I use on a much more regular basis, in this case HTML and CSS.
+
+## Instructions
+
+#### Browser
+
+1. Create an HTML file using cv.html as an example
+
+1. Open the file in your browser
+
+1. Print to PDF
+
+#### Node.js
+
+1. Create an HTML file using cv.html as an example
 
 1. Install [Node.js](https://nodejs.org)
 
-2. Install prerequisites for build script
+1. Install prerequisites for build script
 
-    ```
-    npm install
-    ```
+   ```
+   npm install
+   ```
 
-3. Modify cv.html as desired
+1. Run build script
 
-4. Run build script
+   ```
+   npm start /path/to/file.html
+   ```
 
-    ```
-    npm start
-    ```
+   Or:
 
-    Or:
-    ```
-    node .
-    ```
+   ```
+   node . /path/to/file.html
+   ```
 
+## Page breaks
 
-### Page breaks
-
-Page breaks should not occur in `subsection` elements. If the page breaks at an undesired spot, you can manually insert a page break by adding `<div style="page-break-after: always;"></div>`for example:
+Page breaks should not occur in `subsection` elements. If the page breaks at an undesired spot, you can manually insert a page break by adding `<div style="page-break-after: always;"></div>`, for example:
 
 ```html
 <div class="subsection">
@@ -39,10 +52,10 @@ Page breaks should not occur in `subsection` elements. If the page breaks at an 
 <div style="page-break-after: always;"></div>
 <div class="subsection">
   <!-- ... -->
+</div>
 ```
 
-
-### Create preview image
+## Create preview image
 
 ```
 pdftoppm cv.pdf preview -singlefile -r 100 -png
--- build.js
+++ build.js
@@ -1,52 +1,44 @@
-const fs = require('fs');
 const path = require('path');
 const puppeteer = require('puppeteer');
 
 const INPUT_EXTENSION = '.html';
 const OUTPUT_EXTENSION = '.pdf';
 
-// https://superuser.com/a/1250638/93066
-function timeout(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+const main = async () => {
+  if (process.argv.length !== 3) {
+    console.log('Error: Please provide the HTML file path');
+    process.exit(1);
+  }
+
+  const inputFile = process.argv[2];
+
+  await printToPdf(inputFile);
 };
 
 // https://developers.google.com/web/updates/2017/04/headless-chrome#screenshots
-async function printToPdf(browser, inputUrl) {
-  const outputFilename = path.basename(inputUrl, INPUT_EXTENSION) + OUTPUT_EXTENSION;
+async function printToPdf(inputFile) {
+  const inputFileUrl = `file://${path.join(__dirname, inputFile)}`;
+
+  const browser = await puppeteer.launch();
+  const outputFilename =
+    path.basename(inputFileUrl, INPUT_EXTENSION) + OUTPUT_EXTENSION;
   const page = await browser.newPage();
 
-  await page.goto(inputUrl);
+  await page.goto(inputFileUrl);
   // Waiting allows time for the remote fonts to load
   await timeout(1500);
   await page.pdf({
     path: outputFilename,
     // Print background images, used for horizontal lines before section titles
-    printBackground: true
+    printBackground: true,
   });
-}
 
-async function printFilesToPdf(files) {
-  const browser = await puppeteer.launch();
-
-  for (let i = 0; i < files.length; i++) {
-    const filename = files[i];
-    if (filename.endsWith(INPUT_EXTENSION)) {
-      await printToPdf(browser, `file:${path.join(__dirname, filename)}`);
-    }
-  }
   await browser.close();
 }
 
-function getFilesInCwd() {
-  return new Promise((resolve, reject) => {
-    fs.readdir(__dirname, (err, files) => {
-      if (err) reject(err);
-      resolve(files);
-    })
-  })
+// https://superuser.com/a/1250638/93066
+function timeout(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-(async() => {
-  const filesInCwd = await getFilesInCwd();
-  await printFilesToPdf(filesInCwd);
-})();
+main();
